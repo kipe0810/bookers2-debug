b0e55b09ddf47dd30377a2ba4775073d5338d598
kippeinakade <tkrs23280810@gmail.com>
2020-01-11 04:34:08 +0000

    finish bookers2-debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    user_path(current_user.id)
+    user_path(resource)
   end
   def after_sign_up_path_for(resource)
-    user_path(current_user.id)
+    user_path(resource)
   end
 
   #sign_out後のredirect先変更する。rootパスへ。rootパスはhome topを設定済み。
